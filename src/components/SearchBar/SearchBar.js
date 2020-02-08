561cde2f82b452a16a6b145fac7d1fed5bdaff8d
Roberto Ross Leon <dev@rross.me>
2020-02-08 12:13:41 -0600

    Search button now gives auth when starting. Blank search issue fixed.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,7 +4,8 @@ export class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: ''
+      term: '',
+      auth: false
     }
 
     this.handleTermChange = this.handleTermChange.bind(this);
@@ -24,7 +25,9 @@ export class SearchBar extends React.Component {
       // Authorized
       // Request a Spotify sarch
       const token = this.props.auth.token;
-      this.props.searchSpotify(this.state.term, token);
+      if (this.state.term !== '') {
+        this.props.searchSpotify(this.state.term, token);
+      }
     } else {
       // Non Authorized
       // Request Login
@@ -44,7 +47,7 @@ export class SearchBar extends React.Component {
       <form onSubmit={this.handleClick} className="SearchBar">
         <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
         <div style={{display: "inline-block"}}>
-          <a onClick={this.handleClick} style={{display: "inline-block", marginRight: "15px"}}>SEARCH</a>
+          <a onClick={this.handleClick} style={{display: "inline-block", marginRight: "15px"}}>{this.props.auth.auth?'SEARCH':'AUTHORIZE'}</a>
           <a onClick={this.handleRecommendation} style={{display: "inline-block", marginLeft: "15px"}}>RECOMMEND</a>
         </div>
       </form>
